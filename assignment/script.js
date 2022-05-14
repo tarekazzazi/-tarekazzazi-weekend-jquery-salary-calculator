57cc1c576e826903cf4bb54e1f7292f4981b16a1
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-05-14 09:30:45 -0500

    Grabbed Employee inputs

--- assignment/script.js
+++ assignment/script.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
     console.log('ready!');
     
+
+    $('#SubmitBtn').on('click', AddEmployeeInfo);
+
+
+
+
+
+
+
+
+
 });
 
+function  AddEmployeeInfo() {
+    console.log('In AddEmployeeInfo');
+    $('#firstName').val();
+    $('#lastName').val();
+    $('#ID').val();
+    $('#jobTitle').val();
+    $('#annualSalary').val();
+
+  
+}
