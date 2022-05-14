2e239c57c906c05426f4dcfae25f5b960487e89c
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-05-14 18:40:28 -0500

    delete btn added

--- assignment/script.js
+++ assignment/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     console.log('ready!');
-    
 
-    $('#SubmitBtn').on('click', AddEmployeeInfo);
-
+    $(document).on('click', '#SubmitBtn', AddEmployeeInfo);
+    $(document).on('click', '.delete-btn' , onDelete); 
+        
 
 
 
@@ -16,11 +16,42 @@ $(document).ready(function () {
 
 function  AddEmployeeInfo() {
     console.log('In AddEmployeeInfo');
-    $('#firstName').val();
-    $('#lastName').val();
-    $('#ID').val();
-    $('#jobTitle').val();
-    $('#annualSalary').val();
 
-  
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let ID = $('#ID').val();
+    let jobTitle = $('#jobTitle').val();
+    let annuelSalary = $('#annualSalary').val();
+
+    $('#employeesTable').append(`
+    <tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${ID}</td>
+        <td>${jobTitle}</td>
+        <td>${annuelSalary}</td>
+
+        <td>
+            <button class="delete-btn">
+                delete 
+            </button>
+        </td>
+    </tr>
+`)
+
+$('#firstName').val('');
+$('#lastName').val('');
+$('#ID').val('');
+$('#jobTitle').val('');
+$('#annualSalary').val('');
+
+}
+
+function onDelete() {
+    console.log('in onDelete', $(this));
+
+    let tr = $(this)
+    .parent().parent()
+    
+    tr.remove();
 }
