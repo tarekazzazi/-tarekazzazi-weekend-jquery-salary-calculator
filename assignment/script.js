ec7b2045db17df1883b28fac64bf07d1851bd40d
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-05-15 13:21:33 -0500

    reorganized a little nicer

--- assignment/script.js
+++ assignment/script.js
@@ -1,4 +1,4 @@
-let tableEmployees = [ ];
+let tableEmployees = [];
 
 let salarytotals = [];
 
@@ -23,24 +23,38 @@ function AddEmployeeInfo() {
     console.log('In AddEmployeeInfo');
 
     // I could not understand how to access the annualSalary colum fromm my html table that I created. 
+    // gets input firstName  assigns it to variable called firstName
 
-    let firstName = $('#firstName').val(); // gets input firstName  assigns it to variable called firstName
+    let firstName = $('#firstName').val(); 
+
     let lastName = $('#lastName').val();
+
     let ID = $('#ID').val();
+
     let jobTitle = $('#jobTitle').val();
+
     let annuelSalary = $('#annualSalary').val();
 
-    newEmployee = { // I tried to take in the values entered and create a object called newEmployee with them which I pushed to a array called tableEmployees.  Simular to the Object-group-bonus calculator. I had trouble getting the annual salary values from tableEmployees array.
+    newEmployee = { 
+        // I tried to take in the values entered and create a object called newEmployee with them which I pushed to a array called tableEmployees.  Simular to the Object-group-bonus calculator. I had trouble getting the annual salary values from tableEmployees array.
+        
         firstName: $('#firstName').val(),
+
         lastName: $('#lastName').val(),
+
         ID: $('#ID').val(),
+
         jobTitle: $('#jobTitle').val(),
+
         annuelSalary: $('#annualSalary').val(),
     }
 
-    tableEmployees.push(newEmployee); // added newEmployee object to tableEmployees array
+    tableEmployees.push(newEmployee); 
+    
+    // added newEmployee object to tableEmployees array
 
     // Adds inputs from variables assigned above and appends them to the DOM is a table format
+
     $('#employeesTable').append(`
     <tr>
         <td>${firstName}</td> 
@@ -58,18 +72,24 @@ function AddEmployeeInfo() {
 `)
 
     $('#firstName').val('');
+
     $('#lastName').val('');
+
     $('#ID').val('');
+
     $('#jobTitle').val('');
+
     $('#annualSalary').val('');
 
     calcAnnualSalary();
 }
 
 function onDelete() {
+
     console.log('in onDelete', $(this));
 
     let tr = $(this)
+
         .parent().parent()
     // assigns tr variable to this parent tr parent td
 
@@ -77,21 +97,27 @@ function onDelete() {
     //removes the row with the delete button on it
 }
 
-function findEmployeeSalary(employeeArray) { // takes in array of newEmployee objects and calculate the salary for each of them
+function findEmployeeSalary(employeeArray) { 
+
+    // takes in array of newEmployee objects and calculate the salary for each of them
     // for every employee in the employeeArray 
+
+
     for (const employee of employeeArray) {
+
         let res = calcAnnualSalary(employee)
-        
+
     }
-    
+
 }
 
 function calcAnnualSalary(employeeObject) {
 
     // lets totalSalary variable = array of employee objects which it then goes inside newEmployee and then annuelSalary
-    let totalSalary = (tableEmployees.newEmployee.annuelSalary); // I'm not sure why I get undefined 
     
-        let value = Number(totalSalary); // changes totalSalary from string to Number
+    let totalSalary = (tableEmployees.newEmployee.annuelSalary); // I'm not sure why I get undefined 
+
+    let value = Number(totalSalary); // changes totalSalary from string to Number
 
     console.log('the salary is', value);
 
@@ -104,10 +130,12 @@ function calcAnnualSalary(employeeObject) {
 
 for (employee of tableEmployees) {
 
+    // Checks to see if variable total salary is greater than 20000 
 
-// Checks to see if variable total salary is greater than 20000 
     if (totalSalary > 20000) {
+
         // If true change the id total background-color to red
+        
         $('#total').css('background-color', 'red');
     }
 }
